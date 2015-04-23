3dd92655a3aeb45d3030dce288d33c1272c68924
Brent Bailey & Jeremy Greenwood <student@epicodus.com>
2015-04-23 16:21:07 -0700

    Add and pass Sinatra integration specs

--- spec/allergen_spec_integration.rb
+++ spec/allergen_spec_integration.rb
@@ -4,4 +4,10 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 describe('the result path', {:type => :feature}) do
-  it('informs user of their allergies')
+  it('informs user of their allergies') do
+    visit('/')
+    fill_in('input', :with => 87)
+    click_button('Send')
+    expect(page).to have_content('pollen', 'tomatoes', 'peanuts', 'shellfish', 'eggs')
+  end
+end
